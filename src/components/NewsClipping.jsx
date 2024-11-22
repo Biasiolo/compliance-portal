6b7b701a702a4ec2f788fa7075143d5b14d44551
Biasiolo <biasiolorafael@gmail.com>
2024-11-22 15:51:44 -0300

    initial

--- src/components/NewsClipping.jsx
+++ src/components/NewsClipping.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import '../styles/news.scss';
+import '../styles/News.scss';
 
 import BannerImage from '../assets/test.png';
 
